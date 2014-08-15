13e5892dc63992fd3402821f351cbb40c28b5868
cgcgbcbc <cgcgbcbc@163.com>
2014-08-15 16:30:42 +0800

    Initial Commit

---
+++ CategoryDemo/CDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAppDelegate.h
+//  CategoryDemo
+//
+//  Created by Guang Chen on 8/15/14.
+//  Copyright (c) 2014 Guang Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CategoryDemoTests/CategoryDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  CategoryDemoTests.m
+//  CategoryDemoTests
+//
+//  Created by Guang Chen on 8/15/14.
+//  Copyright (c) 2014 Guang Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface CategoryDemoTests : XCTestCase
+
+@end
+
+@implementation CategoryDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
